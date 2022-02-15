5ddf89f841ad1e300c02f2472cf3672f4073fa42
Alejandro Hernández <hernandezlara180@gmail.com>
2022-02-15 17:25:15 -0600

    Update clientesAdminController.php

--- app/Http/Controllers/clientesAdminController.php
+++ app/Http/Controllers/clientesAdminController.php
@@ -4,36 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
-
 use DB;
-
-
-    /** 
-     * 
-     * @param
-     * @return
-     * 
-     * public function index(){
-
-      *  $clientesAdminV = DB::table('usuarios')->get();
-
-       * return view('clientesAdmin', $clientesAdminV);
-        
-
-    *}
-
-     * 
-     * return view('clientesAdmin', $clientesAdminV);
-     * 
-     * **/
-
 
 class clientesAdminController extends Controller{
 
 
     public function index()
     {
- 
         $clientes = DB::select('CALL `fungdb`.`mostrar_clientes`();');
         $clientes2 = DB::select('CALL `fungdb`.`mostrar_clientes`();');
         return view('clientesAdmin', [ "clientesAdmin" => $clientes,"clientesAdmin2" => $clientes2 ]);
@@ -42,7 +19,7 @@ class clientesAdminController extends Controller{
     public function update(Request $request, $id)
     {
         DB::select('CALL `fungdb`.`modificar_usuario`("'.
-        $request->input('Id').'","'.
+        $id.'","'.
         $request->input('Nombre').'","'.
         $request->input('Correo').'");');
         return redirect()->route('clientesAdmin.index');
